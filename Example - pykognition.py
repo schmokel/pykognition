966e35abd7b29e76f1e78c3892fab99913b5dd9e
schmokel <rasmusschmokel@gmail.com>
2020-10-10 19:35:33 +0200

    updated example script

--- Example - pykognition.py
+++ Example - pykognition.py
@@ -8,8 +8,6 @@ Created on Fri Apr 10 13:27:09 2020
 import yaml
 import pykognition as pykog
 import os
-import pandas as pd
-import re
 
 
 _root = os.getcwd()
@@ -20,33 +18,52 @@ _credentials = yaml.load(open(os.path.join(os.getcwd(), 'Credentials.yaml')))
 _personal_access_key = _credentials['amazon']['personal_access_key']
 _secret_access_key = _credentials['amazon']['secret_access_key']
 
+#T he simpler os.listdir           
+#local_images = os.listdir(input_path)
 
+# or recursively reading files and files in subfolders in 'input_path'
 local_images = []
 for root, dirs, files in os.walk(_root + input_path):
     for file in files:
         if(file.endswith(".jpg")):
             local_images.append(os.path.join(root, file))
             
-            
-            
-#local_images = os.listdir(input_path)
-local_images = [image for image in local_images if "123003499150370" in image]
 
+
+
+######################################
+#                                    #
+# Face and emotion detection-class   #
+#                                    #
+######################################
+
+#input PAK and SAK
 ifa = pykog.ImageFaceAnalysis(_personal_access_key, _secret_access_key)
 
+#initialize, that is actually running, the analysis
 ifa.initialize(imageFileList = local_images)
 
-#ifa_df = ifa.get(attributes = ['emotions', 'features', 'age'])
+#Extract  all attributes
+ifa_df = ifa.get(attributes = ['emotions', 'features', 'age'])
+
+#or just emotions
 ifa_df = ifa.get(attributes = ['emotions'])
 
 
+#get images with face-boxes
 help(ifa.draw)
-out = r"C:/Users/rasmu/Documents/Repositories/pykognition/outputEmotions/"
+out = _root + "/outputEmotions/"
 ifa.draw(outputPath = out, conf_threshold = 80, font_size = 24)
 
 #%%
 
+#################################
+#                               #
+# Object detection-class (BETA) #
+#                               #
+#################################
+
 ioa = pykog.ImageObjectAnalysis(_personal_access_key, _secret_access_key)
-ioa.initialize(inputPath = input_path, imageFileList = local_images)
+ioa.initialize(imageFileList = local_images)
 
 ioa_df = ioa.get()
